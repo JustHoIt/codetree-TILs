fec08c6a7dadf61c442ab31cef7411b08aa7e2f2
박호민 <106057532+JustHoIt@users.noreply.github.com>
2023-12-15 14:13:38 +0900

    Title: 작은 구슬의 이동, Time: 124ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 231215/작은 구슬의 이동/small-marble-movement.java
+++ 231215/작은 구슬의 이동/small-marble-movement.java
@@ -1,44 +1,33 @@
 import java.util.Scanner;
 
 public class Main {
-    static int[] dx = {0, 0, 1, -1};
-    static int[] dy = {1, -1, 0, 0};
+    static int[] dx = {0, 1, -1, 0};
+    static int[] dy = {1, 0, 0, -1};
     static int n = 0, t = 0;
     static int x = 0, y = 0;
 
     public static int getDir(String a) {
         char d = a.charAt(0);
-        if (d == 'U') {
+        if (d == 'R') {
             return 0;
         } else if (d == 'D') {
             return 1;
-        } else if (d == 'R') {
+        } else if (d == 'U') {
             return 2;
         } else {
             return 3;
         }
     }
-
-    public int cngDir(int d) {
-        if (d == 0) {
-            return 1;
-        } else if (d == 1) {
-            return 0;
-        } else if (d == 2) {
-            return 3;
-        } else {
-            return 2;
-        }
-    }
-
 
     public void solution(int r, int c, int d) {
         for (int i = 0; i < t; i++) {
-            if (x > 0 && x < n - 1 && y > 0 && y < n - 1) {
-                x = x + dx[d];
-                y = y + dy[d];
+            int nx = x + dx[d];
+            int ny = y + dy[d];
+            if (nx >= 0 && nx < n  && ny >= 0 && ny < n) {
+                x = nx;
+                y = ny;
             } else {
-                d = cngDir(d);
+                d = 3 - d;
             }
         }
 
