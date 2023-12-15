f5c6a588a6542e34922233615470319a7a79f232
박호민 <106057532+JustHoIt@users.noreply.github.com>
2023-12-15 15:36:56 +0900

    Title: 되돌아오기, Time: 125ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231215/되돌아오기/come-back.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+public class Main {
+    static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {1, 0, -1, 0};
+    static int n;
+    static int[] move;
+    static int[] dir;
+    static int cnt = 0;
+    static int answer = 0;
+
+
+    static int x = 0, y = 0;
+
+    public static int dirNum(String dir) {
+        if (dir.equals("N")) {
+            return 0;
+        } else if (dir.equals("E")) {
+            return 1;
+        } else if (dir.equals("S")) {
+            return 2;
+        } else {
+            return 3;
+        }
+    }
+
+
+    public static int solution() {
+        for (int i = 0; i < n ; i++) {
+            for (int j = 0; j < move[i] ; j++) {
+                int dirN = dir[i];
+                x = x + dx[dirN];
+                y = y + dy[dirN];
+                cnt++;
+
+                if(x == 0 && y == 0){
+                    answer = cnt;
+                    return answer;
+                }
+            }
+        }
+        return answer;
+    }
+
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        dir = new int[n];
+        move = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            String dirw = sc.next();
+            dir[i] = dirNum(dirw);
+            move[i] = sc.nextInt();
+        }
+
+        System.out.println(T.solution());
+    }
+}
