59bb8428ea8dfe420b68fbf08d354468604dcd61
박호민 <106057532+JustHoIt@users.noreply.github.com>
2023-12-16 18:46:12 +0900

    Title: 빙빙 돌며 사각형 채우기, Time: 313ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 231216/빙빙 돌며 사각형 채우기/snail-alphabet-square.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class Main {
+    static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {1, 0, -1, 0};
+    static int n = 0, m = 0;
+    static char[][] arr;
+
+    static int x = 0, y = 0;
+    static int dir = 0;
+
+    public static void solution() {
+        char c = 'B';
+        for (int i = 2; i <= n * m; i++) {
+
+            int nx = x + dx[dir];
+            int ny = y + dy[dir];
+            if (!inRange(nx, ny) || arr[nx][ny] != 0) {
+                dir = (dir + 1) % 4;
+                nx = x + dx[dir];
+                ny = y + dy[dir];
+                while (!inRange(nx, ny)){
+                    dir = (dir + 1) % 4;
+                    nx = x + dx[dir];
+                    ny = y + dy[dir];
+                }
+
+            }
+            x = nx;
+            y = ny;
+            arr[x][y] = c++;
+            if(c > 'Z'){
+                c ='A';
+            }
+        }
+        for (int j = 0; j < n; j++) {
+            for (int k = 0; k < m; k++) {
+                System.out.print(arr[j][k] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static boolean inRange(int nx, int ny) {
+        return (nx >= 0 && nx < n && ny >= 0 && ny < m);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        m = sc.nextInt();
+        arr = new char[n][m];
+        arr[x][y] = 'A';
+        solution();
+    }
+}
