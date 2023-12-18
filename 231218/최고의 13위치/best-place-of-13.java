db9c3f1ebcd67d066f931b6d14aa38eab3d90d50
박호민 <106057532+JustHoIt@users.noreply.github.com>
2023-12-18 11:21:05 +0900

    Title: 최고의 13위치, Time: 162ms, Memory: 10MB, Status: Passed - CodeTree

--- 231218/최고의 13위치/best-place-of-13.java
+++ 231218/최고의 13위치/best-place-of-13.java
@@ -3,9 +3,9 @@ public class Main {
     public void solution(int N, int[][] arr){
         int maxCoin = Integer.MIN_VALUE;
 
-        for(int i = 0; i < N - 3; i++){
+        for(int i = 0; i < N; i++){
             int coin = 0;
-            for(int j = 0; j < N - 3; j++){
+            for(int j = 0; j < N - 2; j++){
                 if(arr[i][j] == 1){
                     coin++;
                 }
