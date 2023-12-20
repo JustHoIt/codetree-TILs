bb497f84c95892b6b3cc231ef52c9248175af6c0
박호민 <106057532+JustHoIt@users.noreply.github.com>
2023-12-20 19:35:51 +0900

    Title: 인접하지 않은 2개의 숫자, Time: 120ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231220/인접하지 않은 2개의 숫자/two-non-adjacent-numbers.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public int solution(int N, int[] arr){
+        int answer = Integer.MIN_VALUE;
+
+
+        for(int i = 0; i< N - 2; i++){
+            for(int j = i + 2; i < N; i++ ){
+                int idx = arr[i] + arr[j];
+                if(answer < idx){
+                    answer = idx;
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        int[] arr = new int[N];
+
+        for(int i = 0; i < N; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println(T.solution(N, arr));
+
+    }
+}
