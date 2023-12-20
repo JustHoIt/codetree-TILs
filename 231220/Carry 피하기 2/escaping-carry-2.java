4fd015ce661258fafee640752a80f22daa954932
박호민 <106057532+JustHoIt@users.noreply.github.com>
2023-12-20 20:21:48 +0900

    Title: Carry 피하기 2, Time: 124ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231220/Carry 피하기 2/escaping-carry-2.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+    public int solution(int n, int[] arr){
+        int answer = Integer.MIN_VALUE;
+
+        for(int i = 0; i < n - 2; i++ ){
+            for(int j = i + 1; j < n - 1; j++ ){
+                for(int k = j + 1; k < n; k++ ){
+                    if((arr[i] % 10) + (arr[j] % 10) + (arr[k] % 10) < 10 &&
+                      (arr[i] % 100) / 10 + (arr[j] % 100) / 10 + (arr[k] % 100) / 10 < 10 &&
+                      (arr[i] / 1000) + (arr[j] / 1000) + (arr[k] / 1000) < 10 &&
+                      (arr[i] / 10) % 10 + (arr[j] / 10) % 10 + (arr[k] / 10) % 10 < 10){
+
+                        int idx = arr[i] + arr[j] + arr[k];
+                        if(answer < idx){
+                            answer = idx;
+                        }
+                    }
+                }
+            }      
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int [] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(T.solution(n, arr));
+    }
+}
