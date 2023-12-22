a732ae4ef4c6b2d29dbda6390387d64264f57d00
박호민 <106057532+JustHoIt@users.noreply.github.com>
2023-12-22 20:38:29 +0900

    Title: 최고의 13위치 2, Time: 135ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231222/최고의 13위치 2/best-place-of-13-2.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Main {
+    static int n;
+    static int [][] arr;
+
+    public static int solution(){
+        int cnt = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n - 2; j++){
+                for(int k = 0; k < n; k++){
+                    for(int l = 0; l < n - 2; l++){
+                        if(i == k && Math.abs(j - 1) <= 2){
+                            continue;
+                        }
+                        int cnt1 = arr[i][j] + arr[i][j+1] + arr[i][j+2];
+                        int cnt2 = arr[k][l] + arr[k][l+1] + arr[k][l+2];
+                        cnt = Math.max(cnt, cnt1 + cnt2);
+        
+                    }
+                }
+            }
+        }
+        return cnt;
+
+    }
+
+
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        arr = new int[n][n];
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println(T.solution());
+
+    
+
+
+    }
+}
