427e75b0f4fd06b9fff0fee4bf9ffa45c5b2bdba
박호민 <106057532+JustHoIt@users.noreply.github.com>
2024-01-22 22:29:09 +0900

    Title: 두 개의 동일한 수열, Time: 158ms, Memory: 10MB, Status: Passed - CodeTree

--- 240122/두 개의 동일한 수열/two-equal-series.java
+++ 240122/두 개의 동일한 수열/two-equal-series.java
@@ -1,24 +1,33 @@
-import java.util.Scanner;
 import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
 
-        int n  = sc.nextInt();
 
-        int [] arr1 = new int[n];
-        int [] arr2 = new int[n];
+        int n = sc.nextInt();
+
+        int[] arr1 = new int[n];
+        int[] arr2 = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr1[i] = sc.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            arr2[i] = sc.nextInt();
+        }
         Arrays.sort(arr1);
         Arrays.sort(arr2);
 
         String s = "Yes";
-        for(int i = 0; i < n; i++){
-            if(arr1[i] != arr2[i]){
+        for (int i = 0; i < n; i++) {
+            if (arr1[i] != arr2[i]) {
                 s = "No";
                 break;
             }
         }
+
         System.out.println(s);
     }
 }
