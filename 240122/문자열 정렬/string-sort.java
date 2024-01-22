41d3e20b2fb35fdba302da8e0879b7b00825bf39
박호민 <106057532+JustHoIt@users.noreply.github.com>
2024-01-22 22:04:39 +0900

    Title: 문자열 정렬, Time: 146ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240122/문자열 정렬/string-sort.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.next();
+
+        char [] c = s.toCharArray();
+        Arrays.sort(c);
+        s = new String(c);
+        System.out.println(s);
+        
+    }
+}
