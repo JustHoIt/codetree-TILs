28a41b12e1bc9b44882382af6d1cac36bcd5c7f0
박호민 <106057532+JustHoIt@users.noreply.github.com>
2024-01-25 05:09:29 +0900

    Title: 총점 비교, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240125/총점 비교/compare-total-points.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        Student[] students = new Student[n];
+        for(int i = 0; i < n; i++){
+            String name = sc.next();
+            int kor = sc.nextInt();
+            int eng = sc.nextInt();
+            int math = sc.nextInt();
+
+            students[i] = new Student(name, kor, eng, math);
+        }
+        Arrays.sort(students);
+
+        for(int i = 0; i < n; i++){
+            System.out.println(students[i].name + " " + students[i].kor + " " + students[i].eng + " " + students[i].math);
+        }
+    }
+    
+}
+
+class Student implements Comparable<Student>{
+    String name;
+    int kor;
+    int eng;
+    int math;
+    public Student(String name, int kor, int eng, int math){
+        this.name = name;
+        this.kor = kor;
+        this.eng = eng;
+        this.math =  math;
+    }
+
+    @Override
+    public int compareTo(Student o){
+        return (this.kor + this.eng + this.math) - (o.kor + o.eng + o.math);
+    }
+}
