1e40be8b8c75ef0d6dfd2ea7280c9c6f00de20e2
박호민 <106057532+JustHoIt@users.noreply.github.com>
2024-01-28 17:37:46 +0900

    Title: 연속되는 수 3, Time: 132ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240128/연속되는 수 3/continuous-number3.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int answer = 0, cnt = 0;
+
+        int [] arr = new int[N];
+        for(int i = 0; i < N; i++){
+            arr[i] = sc.nextInt();
+        }
+        String yn = "N";
+
+        for(int i = 1; i < N; i++){
+            if(i  >=  1 && arr[i] * arr[i - 1] > 0){
+                cnt += 1;
+            }else{
+                cnt = 1;
+            }
+            answer = Math.max(answer, cnt);
+        }
+        System.out.println(answer);
+    }
+}
