ef8b70d606af2c0d800cdbf88791998297da429d
박호민 <106057532+JustHoIt@users.noreply.github.com>
2024-01-28 17:40:18 +0900

    Title: 연속되는 수 3, Time: 176ms, Memory: 11MB, Status: Passed - CodeTree

--- 240128/연속되는 수 3/continuous-number3.java
+++ 240128/연속되는 수 3/continuous-number3.java
@@ -11,7 +11,6 @@ public class Main {
         for(int i = 0; i < N; i++){
             arr[i] = sc.nextInt();
         }
-        String yn = "N";
 
         for(int i = 1; i < N; i++){
             if(i  >=  1 && arr[i] * arr[i - 1] > 0){
