4b4a600417e7c4efb70db7f1c898589cfa11d062
박호민 <106057532+JustHoIt@users.noreply.github.com>
2024-01-28 17:14:28 +0900

    Title: 연속되는 수 2, Time: 129ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240128/연속되는 수 2/continuous-number2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int answer = 1;
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int [] arr = new int [N];
+        for(int i = 0; i < N; i++){
+            arr[i] = sc.nextInt();
+        }
+
+
+        for(int i = 1; i < N; i++){
+            if(arr[i-1] != arr[i]){
+                answer++;
+            }
+        }
+        System.out.println(answer);
+    }
+}
