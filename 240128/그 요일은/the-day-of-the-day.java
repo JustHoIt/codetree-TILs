7cc1af5993893e27ba693574c38cb231642723f3
박호민 <106057532+JustHoIt@users.noreply.github.com>
2024-01-28 11:55:01 +0900

    Title: 그 요일은, Time: 131ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240128/그 요일은/the-day-of-the-day.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        String[] dayOfWeek = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+        int[] endDays = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        Scanner sc = new Scanner(System.in);
+
+        int m1 = sc.nextInt();
+        int d1 = sc.nextInt();
+        int m2 = sc.nextInt();
+        int d2 = sc.nextInt();
+        String day = sc.next();
+        int cnt = 0;
+        int week = 0;
+        int days = 1;
+        int answer = 0;
+        while(true){
+            if(m1 == m2 && d1 == d2){
+                break;
+            }
+            days++;
+            d1++;
+
+            if(d1 > endDays[m1]){
+                m1++;
+                if(m1 == 13){
+                    m1 = 1;
+                }
+                d1 = 1;
+            }
+        }
+        answer += days / 7;
+        cnt = days % 7;
+
+        for(int i = 0; i <= cnt; i++){
+            if(dayOfWeek[i].equals(day)){
+                answer++;
+                break;
+            }
+        }
+        System.out.println(answer);
+    }
+}
