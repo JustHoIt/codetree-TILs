4715983cf995ba290d74032a1cbb1d05ef81a00e
박호민 <106057532+JustHoIt@users.noreply.github.com>
2024-01-30 16:56:28 +0900

    Title: 그 요일은, Time: 196ms, Memory: 10MB, Status: Passed - CodeTree

--- 240130/그 요일은/the-day-of-the-day.java
+++ 240130/그 요일은/the-day-of-the-day.java
@@ -1,20 +1,21 @@
 import java.util.Scanner;
 
 public class Main {
+    static int m1, m2, d1, d2 ;
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         String[] dayOfWeek = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
         Scanner sc = new Scanner(System.in);
 
-        int m1 = sc.nextInt();
-        int d1 = sc.nextInt();
-        int m2 = sc.nextInt();
-        int d2 = sc.nextInt();
+        m1 = sc.nextInt();
+        d1 = sc.nextInt();
+        m2 = sc.nextInt();
+        d2 = sc.nextInt();
 
 
         String day = sc.next();
 
-        int diff = numOfDays(m2, d2) - numOfDays(m1, d1);
+        int diff = numOfDays();
         int answer = diff / 7;
         int cnt = diff % 7;
 
@@ -34,15 +35,21 @@ public class Main {
         System.out.println(answer);
     }
 
-    public static int numOfDays(int m, int d){
+    public static int numOfDays(){
         int[] endDays = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-        int totalDays = 0;
+        int totalDays = 1;
 
-        for(int i = 1; i < m; i++){
-            totalDays += endDays[i];
+        while (true){
+            if(m1 == m2 && d1 == d2){
+                break;
+            }
+            totalDays++;
+            d1++;
+            if(d1 > endDays[m1]){
+                m1++;
+                d1 = 1;
+            }
         }
-        totalDays += d;
-
         return totalDays;
     }
 }
