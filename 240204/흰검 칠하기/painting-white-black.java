f8e05460aea89035a6578c73b223e7d29d905637
박호민 <106057532+JustHoIt@users.noreply.github.com>
2024-02-04 21:16:24 +0900

    Title: 흰검 칠하기, Time: 293ms, Memory: 16MB, Status: Passed - CodeTree

--- 240204/흰검 칠하기/painting-white-black.java
+++ 240204/흰검 칠하기/painting-white-black.java
@@ -1,69 +1,45 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-    static int MAX_VALUE = 2000001;
-    static int offset = 1000000;
+    public static final int MAX_K = 100000;
+    
+    public static int n;
+    public static int[] a = new int[2 * MAX_K + 1];
+    public static int[] cntB = new int[2 * MAX_K + 1];
+    public static int[] cntW = new int[2 * MAX_K + 1];
+    public static int b, w, g;
 
     public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        n = sc.nextInt();
 
-        String[] checked = new String[MAX_VALUE];
-        Arrays.fill(checked, "");
-        int[] x1 = new int[1000];
-        int[] x2 = new int[1000];
-        String[] s1 = new String[1000];
-        int[] ckw = new int[MAX_VALUE];
-        int[] ckb = new int[MAX_VALUE];
-
-        int b = 0, w = 0, g = 0; // black = b, white = w. gray = g
-        int current = 0;  //현재 위치
-
-        for (int i = 0; i < n; i++) {
-            int dis = sc.nextInt();
-            String x = sc.next();
-
-            if (x.equals("L")) {
-                x1[i] = current - dis;
-                x2[i] = current;
-                s1[i] = "w";
-                current -= dis;
-            } else {
-                x1[i] = current;
-                x2[i] = current + dis;
-                s1[i] = "b";
-                current += dis;
+        int cur = MAX_K;
+        for(int i = 1; i <= n; i++) {
+            int x = sc.nextInt();
+            char c = sc.next().charAt(0);
+            if(c == 'L') {
+                while(x-- > 0) {
+                    a[cur] = 1;
+                    cntW[cur]++;
+                    if(x > 0) cur--;
+                }
             }
-        }
-
-        for (int i = 0; i < n; i++) {
-            for (int j = x1[i] + offset; j < x2[i] + offset; j++) {
-                checked[j] = s1[i];
-                if (s1[i].equals("w")) {
-                    ckw[j]++;
-                } else if (s1[i].equals("b")) {
-                    ckb[j]++;
+            else {
+                while(x-- > 0) {
+                    a[cur] = 2;
+                    cntB[cur]++;
+                    if(x > 0) cur++;
                 }
             }
         }
-        for (int i = 0; i < MAX_VALUE; i++) {
-            if (ckb[i] + ckw[i] > 3) {
-                checked[i] = "g";
-            }
+
+        for(int i = 0; i <= 2 * MAX_K; i++) {
+            if(cntB[i] >= 2 && cntW[i] >= 2) g++;
+            else if(a[i] == 1) w++;
+            else if(a[i] == 2) b++;
         }
-        for (int i = 0; i < MAX_VALUE; i++) {
-            if (!checked[i].isEmpty()) {
-                if (checked[i].equals("b")) {
-                    b++;
-                } else if (checked[i].equals("w")) {
-                    w++;
-                } else if (checked[i].equals("g")) {
-                    g++;
-                }
-            }
-        }
-        System.out.println(w + " " + b + " " + g);
+
+        // 정답을 출력합니다.
+        System.out.print(w + " " + b + " " + g);
     }
 }
