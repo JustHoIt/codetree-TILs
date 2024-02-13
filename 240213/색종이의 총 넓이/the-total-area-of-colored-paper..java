1ea27d3d09c1b5e8bdae8fee87a184bc487ea90e
박호민 <106057532+JustHoIt@users.noreply.github.com>
2024-02-13 21:20:35 +0900

    Title: 색종이의 총 넓이, Time: 150ms, Memory: 10MB, Status: Passed - CodeTree

--- 240213/색종이의 총 넓이/the-total-area-of-colored-paper..java
+++ 240213/색종이의 총 넓이/the-total-area-of-colored-paper..java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
 
-    static int OFFSET = 8;
+    static int OFFSET = 100;
     static int MAX = 200;
     static int[][] checked = new int[MAX + 1][MAX + 1];
 
@@ -11,10 +11,10 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         for (int i = 0; i < n; i++) {
-            int x1 = sc.nextInt();
-            int y1 = sc.nextInt();
-            int x2 = x1 + OFFSET;
-            int y2 = y1 + OFFSET;
+            int x1 = sc.nextInt() + OFFSET;
+            int y1 = sc.nextInt() + OFFSET;
+            int x2 = x1 + 8;
+            int y2 = y1 + 8;
 
             for (int j = x1; j < x2; j++) {
                 for (int k = y1; k < y2; k++) {
@@ -22,7 +22,7 @@ public class Main {
                 }
             }
         }
-        
+
         for (int i = 0; i <= MAX; i++) {
             for (int j = 0; j <= MAX; j++) {
                 if (checked[i][j] >= 1) {
