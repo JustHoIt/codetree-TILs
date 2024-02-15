06040002dfcf9dec3edc470ee188e5b139e33c53
박호민 <106057532+JustHoIt@users.noreply.github.com>
2024-02-15 23:36:52 +0900

    Title: G or H 3, Time: 346ms, Memory: 176MB, Status: Memory Limit Exceed - CodeTree

--- 240215/G or H 3/G-or-H-3.java
+++ 240215/G or H 3/G-or-H-3.java
@@ -2,28 +2,31 @@ import java.util.Scanner;
 
 public class Main {
     static int MAX_VALUE = 100000000;
-    static char [] line = new char[MAX_VALUE + 1];
+    static char[] line = new char[MAX_VALUE + 1];
 
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         int K = sc.nextInt();
-        int maxNum = 1;
+        int maxV = 0;
+        int maxNum = 0;
         for (int i = 0; i < N; i++) {
             int l = sc.nextInt();
+            maxV = Math.max(maxV, l);
             line[l] = sc.next().charAt(0);
         }
-        for(int i = 1; i <= K; i++){
+        for (int i = 1; i <= maxV - K + 1; i++) {
             int num = 0;
-            if(line[i] == 'G'){
-                num = 1;
-            } else if (line[i] == 'H') {
-                num = 2;
+            for (int j = i; j <= i + K; j++) {
+                if (line[j] == 'G') {
+                    num += 1;
+                } else if (line[j] == 'H') {
+                    num += 2;
+                }
             }
-            maxNum += num;
+            maxNum = Math.max(maxNum, num);
         }
-        System.out.println(1);
         System.out.println(maxNum);
     }
 }
