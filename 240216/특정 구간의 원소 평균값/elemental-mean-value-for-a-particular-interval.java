8f6571d55a3e124c4d8eb965f167c74fea041066
박호민 <106057532+JustHoIt@users.noreply.github.com>
2024-02-16 23:46:50 +0900

    Title: 특정 구간의 원소 평균값, Time: 184ms, Memory: 10MB, Status: Passed - CodeTree

--- 240216/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.java
+++ 240216/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 
 public class Main {
+    public static final int MAX_N = 100;
+    public static int[] arr = new int[MAX_N];
+
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
@@ -10,16 +13,14 @@ public class Main {
             arr[i] = sc.nextInt();
         }
         int cnt = 0;
-        cnt += n;
         for (int i = 0; i < n; i++) {
-            for (int j = i; j < n; j++) {
-                boolean ex = false;
+            for (int j = i; j < n; j++) {  
                 int sum = 0;
-                double avg = 0.0;
                 for (int k = i; k <= j; k++) {
                     sum += arr[k];
                 }
-                avg = (double) sum / j - i + 1;
+                boolean ex = false;
+                double avg = (double) sum / (j - i + 1);
                 for (int k = i; k <= j; k++) {
                     if (arr[k] == avg) {
                         ex = true;
