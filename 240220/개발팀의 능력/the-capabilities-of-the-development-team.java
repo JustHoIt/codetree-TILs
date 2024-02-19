dc0ac873589b10b7e1bfbfc400954f70d8207e53
박호민 <106057532+JustHoIt@users.noreply.github.com>
2024-02-20 04:07:52 +0900

    Title: 개발팀의 능력, Time: 145ms, Memory: 10MB, Status: Passed - CodeTree

--- 240220/개발팀의 능력/the-capabilities-of-the-development-team.java
+++ 240220/개발팀의 능력/the-capabilities-of-the-development-team.java
@@ -2,21 +2,21 @@ import java.util.Scanner;
 
 public class Main {
     static int N = 5;
-    static int [] developers = new int[N];
+    static int[] developers = new int[N];
     static int total = 0;
 
-    public static int getDiff(int i, int j, int k){
+    public static int getDiff(int i, int j, int k) {
         int sum1 = developers[i] + developers[j];
         int sum2 = developers[k];
         int sum3 = total - (sum1 + sum2);
 
-        if(sum1 == sum2 || sum2 == sum3 || sum1 == sum3){
+        if (sum1 == sum2 || sum2 == sum3 || sum1 == sum3) {
             return total;
         }
         int diff = Math.abs(sum1 - sum2);
 
-        diff = Math.max(diff,Math.abs(sum2 - sum3));
-        diff = Math.max(diff,Math.abs(sum1 - sum3));
+        diff = Math.max(diff, Math.abs(sum2 - sum3));
+        diff = Math.max(diff, Math.abs(sum1 - sum3));
 
         return diff;
     }
@@ -25,27 +25,38 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
 
-        for(int i = 0; i < N; i++){
+        for (int i = 0; i < N; i++) {
             developers[i] = sc.nextInt();
             total += developers[i];
         }
 
-        int minDiff = Integer.MAX_VALUE;
-
-        for(int i = 0; i < N; i++){
+        boolean diffYn = true;
+        for(int i = 0; i < 1; i++){
+            int cnt = 0;
             for(int j = i + 1; j < N; j++){
-                for(int k = 0; k < N; k++){
-                    if(k == i || k == j){
-                        continue;
-                    }
-                    minDiff = Math.min(minDiff, getDiff(i, j, k));
+                if(developers[i] == developers[j]){
+                    cnt++;
+                }
+                if(cnt > 3){
+                    diffYn = false;
                 }
             }
         }
+        if(diffYn){
+            int minDiff = Integer.MAX_VALUE;
 
-        if(minDiff > 0){
+            for (int i = 0; i < N; i++) {
+                for (int j = i + 1; j < N; j++) {
+                    for (int k = 0; k < N; k++) {
+                        if (k == i || k == j) {
+                            continue;
+                        }
+                        minDiff = Math.min(minDiff, getDiff(i, j, k));
+                    }
+                }
+            }
             System.out.println(minDiff);
-        }else {
+        }else{
             System.out.println(-1);
         }
     }
