95b04841157fe2230a8d03af46d8c185e089bdf6
박호민 <106057532+JustHoIt@users.noreply.github.com>
2024-02-22 02:31:08 +0900

    Title: 겹치지 않는 선분 2, Time: 276ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 240222/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.java
@@ -0,0 +1,52 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class Main {
+    static int OFFSET = 1000000;
+    static int[][] arr;
+    static int n;
+
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        arr = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            arr[i][0] = sc.nextInt() + OFFSET;
+            arr[i][1] = sc.nextInt() + OFFSET;
+        }
+
+        Arrays.sort(arr, Comparator.comparingInt((int[] o) -> o[0])); //배열을 x값으로 오름차순 정렬
+        
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            if (solution(i)) {
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+    }
+
+    public static boolean solution(int i) {
+        boolean answer = true;
+        if (i > 0 && i < n - 1) {
+            if (arr[i][1] >= arr[i + 1][1]) {
+                answer = false;
+            }
+            if (arr[i][1] <= arr[i - 1][1]) {
+                answer = false;
+            }
+        } else if (i == 0) { //i가 0일때
+            if (arr[i][1] >= arr[i + 1][1]) {
+                answer = false;
+            }
+        } else if (i == n - 1) { //1가 배열의 마지막 값
+            if (arr[i][1] <= arr[i - 1][1]) {
+                answer = false;
+            }
+        }
+
+        return answer;
+    }
+}
