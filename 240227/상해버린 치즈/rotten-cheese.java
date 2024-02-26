c47af63ff21b1ee06d4016e5932476a122e86082
박호민 <106057532+JustHoIt@users.noreply.github.com>
2024-02-27 04:08:35 +0900

    Title: 상해버린 치즈, Time: 119ms, Memory: 9MB, Status: Passed - CodeTree

--- 240227/상해버린 치즈/rotten-cheese.java
+++ 240227/상해버린 치즈/rotten-cheese.java
@@ -1,54 +1,86 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt(); // 사람의 수
-        int M = sc.nextInt(); // 치즈의 수
-        int D = sc.nextInt(); // 먹은 기록의 수
-        int S = sc.nextInt(); // 아픈 기록의 수
-        int[][] eatenRecords = new int[D][3]; // 먹은 기록
-        int[][] sickRecords = new int[S][2]; //아픈 기록
-        int[] cheeseCond = new int[M]; // 치즈 상태
-        int[] personCond = new int[N]; // 사람 상태
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int D = Integer.parseInt(st.nextToken());
+        int S = Integer.parseInt(st.nextToken());
+
+        // (p번째 사람, m번째 치즈, t초) 먹음
+        ArrayList<int[]> eaten = new ArrayList<>();
         for (int i = 0; i < D; i++) {
-            eatenRecords[i][0] = sc.nextInt() - 1;
-            eatenRecords[i][1] = sc.nextInt() - 1;
-            eatenRecords[i][2] = sc.nextInt();
+            st = new StringTokenizer(br.readLine());
+            eaten.add(new int[] {Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())});
         }
+
+        // (p번째 사람, t초) 아픔
+        HashMap<Integer, Integer> bad_condition = new HashMap<>();
         for (int i = 0; i < S; i++) {
-            sickRecords[i][0] = sc.nextInt() - 1;
-            sickRecords[i][1] = sc.nextInt();
+            st = new StringTokenizer(br.readLine());
+            bad_condition.put(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
         }
-        for (int i = 0; i < M; i++) {
-            boolean spoilsYn = true;
-            for (int j = 0; j < S; j++) {
-                boolean flag = false;
-                for (int k = 0; k < D; k++) {
-                    if (eatenRecords[k][0] == sickRecords[j][0] && eatenRecords[k][2] < sickRecords[j][1]) {
-                        flag = true;
-                        break;
+
+        // Step1 : 상한 치즈 고르기
+        ArrayList<HashSet<Integer>> gone_cheezes = new ArrayList<>();
+        for (int i = 0; i <= N; i++) {
+            gone_cheezes.add(new HashSet<>());
+        }
+
+        HashMap<Integer, ArrayList<int[]>> eaten_dict = new HashMap<>();
+        // 아프기 전 섭취했다면 ? 주워담는다.
+        for (int[] eat : eaten) {
+            if (!eaten_dict.containsKey(eat[0])) {
+                eaten_dict.put(eat[0], new ArrayList<>());
+            }
+            eaten_dict.get(eat[0]).add(new int[] {eat[1], eat[2]});
+        }
+
+        for (Map.Entry<Integer, Integer> entry : bad_condition.entrySet()) {
+            int bad_p = entry.getKey();
+            int bad_t = entry.getValue();
+
+            // bad_t 이하로, bad_p의 치즈를 gone_cheezes[bad_p]에 주워담는다.
+            if (eaten_dict.containsKey(bad_p)) {
+                for (int[] eat : eaten_dict.get(bad_p)) {
+                    if (eat[1] < bad_t) {
+                        gone_cheezes.get(bad_p).add(eat[0]);
                     }
                 }
-                if (!flag) {
-                    spoilsYn = false;
-                    break;
+            }
+        }
+
+        HashSet<Integer> candi = null;
+        for (HashSet<Integer> gone_cheeze : gone_cheezes) {
+            if (gone_cheeze.size() > 0) {
+                if (candi == null) {
+                    candi = new HashSet<>(gone_cheeze);
+                } else {
+                    candi.retainAll(gone_cheeze);
                 }
             }
-            if (spoilsYn) {
-                cheeseCond[i] = 1;
+        }
+
+        // Step2 : 먹은 사람 세기
+        int max_cnt = 0;
+        if (candi != null) {
+            for (int i : candi) {
+                int cnt = 0;
+                for (ArrayList<int[]> eat_cheezes : eaten_dict.values()) {
+                    for (int[] eat : eat_cheezes) {
+                        if (i == eat[0]) {
+                            cnt += 1;
+                            break;
+                        }
+                    }
+                }
+                max_cnt = Math.max(max_cnt, cnt);
             }
         }
-        for (int i = 0; i < D; i++) {
-            if (cheeseCond[eatenRecords[i][1]] == 1 && eatenRecords[i][2] < 101) {
-                personCond[eatenRecords[i][0]] = 1;
-            }
-        }
-        int cnt = 0;
-        for (int i = 0; i < N; i++) {
-            if (personCond[i] == 1) {
-                cnt++;
-            }
-        }
-        System.out.println(cnt);
+
+        System.out.println(max_cnt);
     }
 }
