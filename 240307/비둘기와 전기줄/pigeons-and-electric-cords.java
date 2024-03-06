47993573e21800db6db0cff654316cfb573393f3
박호민 <106057532+JustHoIt@users.noreply.github.com>
2024-03-07 06:57:25 +0900

    Title: 비둘기와 전기줄, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240307/비둘기와 전기줄/pigeons-and-electric-cords.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc= new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int [][] pigeons = new int[n][2];
+        int [] eWires = new int[n];
+        
+        Arrays.fill(eWires, 2);
+
+        for(int i = 0; i < n; i++){
+            pigeons[i][0] = sc.nextInt();
+            pigeons[i][1] = sc.nextInt();
+        } 
+        int move = 0;
+        for(int i = 0; i < n; i++){
+            int cur = pigeons[i][1];// 비둘기의 현재 위치
+            int num = pigeons[i][0] - 1; //비둘기 번호
+            if(eWires[num] == 2){ // 
+                eWires[num] = cur;
+            }else{
+                if(eWires[num] != cur){
+                    move++;
+                    eWires[num] = cur;
+                }
+            }
+
+        }
+        System.out.println(move);
+    }
+}
