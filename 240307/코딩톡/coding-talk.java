be91a53fdd614b9769ac6d07fdadb301e4461d65
박호민 <106057532+JustHoIt@users.noreply.github.com>
2024-03-07 08:05:39 +0900

    Title: 코딩톡, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240307/코딩톡/coding-talk.java
@@ -0,0 +1,47 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); // 인원 수
+        int m = sc.nextInt(); // 메시지 수
+        int p = sc.nextInt() - 1; // 메시지 번호
+        boolean[] checks = new boolean[27]; //알파벳 A ~ Z 갯수
+        Message[] messages = new Message[n];
+        for (int i = 0; i < n; i++) {
+            String sender = sc.next();
+            int view = sc.nextInt();
+            messages[i] = new Message(sender, view);
+        }
+
+        Arrays.fill(checks, false);
+
+        checks[p - 1] = true;
+        for (int i = p; i < m; i++) {
+            char c = messages[i].sender.charAt(0);
+            checks[c - 'A'] = true;
+        }
+        for (int i = 0; i < n; i++) {
+            char c = (char) (i + 'A');
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (!checks[i]) {
+                char c = (char) (i + 'A');
+                System.out.print(c + " ");
+            }
+        }
+    }
+}
+
+class Message {
+    String sender;
+    int view;
+
+    Message(String sender, int view) {
+        this.sender = sender;
+        this.view = view;
+    }
+}
