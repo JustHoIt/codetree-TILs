e265e9a58583bea4b4ed17ba56a32192c12f4dae
박호민 <106057532+JustHoIt@users.noreply.github.com>
2024-03-10 20:04:38 +0900

    Title: 연속된 숫자 만들기 2, Time: 759ms, Memory: 176MB, Status: Memory Limit Exceed - CodeTree

--- 240310/연속된 숫자 만들기 2/create-consecutive-numbers-2.java
+++ 240310/연속된 숫자 만들기 2/create-consecutive-numbers-2.java
@@ -1,14 +1,41 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
-        int [] arr = new int[3];
-        for(int i = 0; i < 3; i++){
-            arr[i] = sc.nextInt();
+        List<Integer> arr = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            arr.add(sc.nextInt());
         }
+        String s = "";
+        int cnt = 0;
+        while (true) {
+            int diff1 = arr.get(1) - arr.get(0);
+            int diff2 = arr.get(2) - arr.get(1);
+            if (diff1 == 1 && diff2 == 1) {
+                break;
+            }
 
-        System.out.println(2);
+            if (diff1 == 2 || diff2 == 2) {
+                cnt++;
+                break;
+            }
+
+            if (diff1 > diff2) {
+                int tmp = arr.get(1);
+                arr.set(1, arr.get(1) + 2);
+                arr.set(0, tmp);
+                cnt++;
+            } else {
+                int tmp = arr.get(1);
+                arr.set(1, tmp - 1);
+                arr.set(2, tmp);
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
     }
 }
