c1d6bf1f0d756b2472cf368eb070f8fc6baebfa1
박호민 <106057532+JustHoIt@users.noreply.github.com>
2024-03-14 06:19:27 +0900

    Title: 세 수의 최대 곱, Time: 844ms, Memory: 34MB, Status: Passed - CodeTree

---
+++ 240314/세 수의 최대 곱/maximum-product-of-three-numbers.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int answer = 0;
+        Scanner sc = new Scanner(System.in);
+        int zero = 0;
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+            if (arr[i] == 0) {
+                zero++;
+            }
+        }
+        Arrays.sort(arr);
+
+        int num1 = arr[n - 1] * arr[n - 2] * arr[n - 3];
+        int num2 = arr[0] * arr[1] * arr[2];
+        int num3 = arr[0] * arr[1] * arr[n - 1];
+
+        answer = Math.max(num1, Math.max(num2, num3));
+
+        System.out.println(answer);
+    }
+}
