d4eeb42fed6150080f21a3046ed2d5a90591c27e
박호민 <106057532+JustHoIt@users.noreply.github.com>
2024-03-16 21:30:18 +0900

    Title: 움직이는 블록, Time: 312ms, Memory: 20MB, Status: Passed - CodeTree

---
+++ 240316/움직이는 블록/moving-block.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int [] arr = new int[n];
+
+        int avg = 0;
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+            avg += arr[i];
+        }
+
+        avg = avg / n;
+        int move = 0;
+        for(int i = 0; i < n; i++){
+            if(avg > arr[i]){
+                move += (avg - arr[i]);
+            }
+        }
+        System.out.println(move);
+    }
+}
