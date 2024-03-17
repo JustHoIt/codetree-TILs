f7daeb1b508f156582e9acf74eb46535d5465ea3
박호민 <106057532+JustHoIt@users.noreply.github.com>
2024-03-18 07:45:13 +0900

    Title: 2개씩 그룹짓기 2, Time: 947ms, Memory: 52MB, Status: Passed - CodeTree

---
+++ 240318/2개씩 그룹짓기 2/group-of-pairs-2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[2 * n];
+
+        for (int i = 0; i < 2 * n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < n; i++) {
+            int a = Math.abs(arr[i] - arr[i + n]);
+            min = Math.min(a, min);
+        }
+        System.out.println(min);
+    }
+}
