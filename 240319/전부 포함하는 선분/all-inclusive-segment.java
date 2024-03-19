f74a40f6a36a273aa92d651411d0ab05f0ab5803
박호민 <106057532+JustHoIt@users.noreply.github.com>
2024-03-19 11:24:32 +0900

    Title: 전부 포함하는 선분, Time: 144ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240319/전부 포함하는 선분/all-inclusive-segment.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] line = new int[n][3];
+        int[] result = new int[101];
+        int maxV = 0;
+        int maxN = 0;
+        for (int i = 0; i < n; i++) {
+            line[i][0] = sc.nextInt();
+            line[i][1] = sc.nextInt();
+            line[i][2] = line[i][1] - line[i][0]; //거리
+        }
+        for (int i = 0; i < n; i++) {
+            if (maxV < line[i][2]) {
+                maxV = line[i][2];
+                maxN = i;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (i != maxN) {
+                for (int j = line[i][0]; j <= line[i][1]; j++) {
+                    result[j] = 1;
+                }
+            }
+        }
+        int distance = 0;
+        for (int i = 0; i < result.length; i++) {
+            if (result[i] == 1) {
+                distance++;
+            }
+        }
+        System.out.println(distance - 1);
+    }
+}
