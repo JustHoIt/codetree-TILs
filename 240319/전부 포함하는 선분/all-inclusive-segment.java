af107cb2cbc296076ef8d4df7c0297b5da5e82dd
박호민 <106057532+JustHoIt@users.noreply.github.com>
2024-03-19 12:12:00 +0900

    Title: 전부 포함하는 선분, Time: 165ms, Memory: 10MB, Status: Passed - CodeTree

--- 240319/전부 포함하는 선분/all-inclusive-segment.java
+++ 240319/전부 포함하는 선분/all-inclusive-segment.java
@@ -2,37 +2,34 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[][] line = new int[n][3];
-        int[] result = new int[101];
-        int maxV = 0;
-        int maxN = 0;
+        int[] startPoints = new int[n];
+        int[] endPoints = new int[n];
+
         for (int i = 0; i < n; i++) {
-            line[i][0] = sc.nextInt();
-            line[i][1] = sc.nextInt();
-            line[i][2] = line[i][1] - line[i][0]; //거리
+            startPoints[i] = sc.nextInt();
+            endPoints[i] = sc.nextInt();
         }
+
+        int totalMin = 101, totalMax = 0;
         for (int i = 0; i < n; i++) {
-            if (maxV < line[i][2]) {
-                maxV = line[i][2];
-                maxN = i;
-            }
+            if (startPoints[i] < totalMin) totalMin = startPoints[i];
+            if (endPoints[i] > totalMax) totalMax = endPoints[i];
         }
+
+        int answer = totalMax - totalMin;
         for (int i = 0; i < n; i++) {
-            if (i != maxN) {
-                for (int j = line[i][0]; j <= line[i][1]; j++) {
-                    result[j] = 1;
-                }
+            int localMin = 101, localMax = 0;
+            for (int j = 0; j < n; j++) {
+                if (i == j) continue; // 현재 선분을 제외
+                if (startPoints[j] < localMin) localMin = startPoints[j];
+                if (endPoints[j] > localMax) localMax = endPoints[j];
             }
+            int localLength = localMax - localMin;
+            if (localLength < answer) answer = localLength;
         }
-        int distance = 0;
-        for (int i = 1; i < result.length; i++) {
-            if (result[i] == 1 && result[i - 1] != 0) {
-                distance++;
-            }
-        }
-        System.out.println(distance);
+
+        System.out.println(answer);
     }
 }
