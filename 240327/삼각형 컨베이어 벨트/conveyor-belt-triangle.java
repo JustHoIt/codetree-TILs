4188844971652c1875e23ba679aacf71197ffd73
박호민 <106057532+JustHoIt@users.noreply.github.com>
2024-03-27 20:26:00 +0900

    Title: 삼각형 컨베이어 벨트, Time: 123ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240327/삼각형 컨베이어 벨트/conveyor-belt-triangle.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int t = sc.nextInt();
+        int a = 3 * n;
+        int [] arr = new int[a];
+
+        for(int i  = 0; i < a; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        int temp = 0;
+        for(int i = 0; i < t; i++){
+            temp = arr[a - 1];
+            for(int j = a - 1; j >= 1; j--){
+                arr[j] = arr[j - 1];
+            }
+            arr[0] = temp;
+        }
+        int cnt = 0;
+        for(int i = 0; i < a; i++){
+            System.out.print(arr[i] + " ");
+            cnt++;
+            if(cnt % 3 == 0){
+                System.out.println();
+            }
+        }
+    }
+}
