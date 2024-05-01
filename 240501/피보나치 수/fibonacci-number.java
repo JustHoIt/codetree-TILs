a5d955ef0e77a85e2f90b417a2bba0968624137f
박호민 <106057532+JustHoIt@users.noreply.github.com>
2024-05-01 16:02:36 +0900

    Title: 피보나치 수, Time: 153ms, Memory: 10MB, Status: Passed - CodeTree

--- 240501/피보나치 수/fibonacci-number.java
+++ 240501/피보나치 수/fibonacci-number.java
@@ -12,9 +12,17 @@ public class Main {
 
     public static int fibo(int n){
         if(n == 1 || n == 2){
-            return 1;
-        }else{
-            return fibo(n - 2) + fibo(n - 1);
+            return 1; 
         }
+
+        int [] fibo = new int[n + 1];
+        fibo[0] = 1;
+        fibo[1] = 1;
+
+        for(int i = 2; i < n; i++){
+            fibo[i] = fibo[i - 1] + fibo[i - 2];
+        }
+
+        return fibo[n - 1];
     }
 }
