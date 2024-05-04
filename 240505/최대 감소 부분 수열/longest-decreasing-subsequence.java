380959af6dfdee7cc28eabf77dbec82e759d8d12
박호민 <106057532+JustHoIt@users.noreply.github.com>
2024-05-05 05:31:01 +0900

    Title: 최대 감소 부분 수열, Time: 237ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 240505/최대 감소 부분 수열/longest-decreasing-subsequence.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int [] arr = new int[n];
+        int [] dp = new int[n];
+
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        for(int i = 0; i < n; i++){
+            dp[i] = 1;
+            for(int j = 0; j < i; j++){
+                if(arr[j] > arr[i] && dp[j] + 1 > dp[i]){
+                    dp[i] = dp[j] + 1;
+                }
+            }
+        }
+
+        int max = 0;
+        for(int i = 0; i < n; i++){
+            max = Math.max(max, dp[i]);
+        }
+
+        System.out.println(max);
+    }
+}
