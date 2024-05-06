977b01c6bc507c864b792772a765c5a99e15d2b4
박호민 <106057532+JustHoIt@users.noreply.github.com>
2024-05-07 07:28:43 +0900

    Title: 산책, Time: 139ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240507/산책/walk.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+
+        HashMap<String, Integer> hMap = new HashMap<>();
+
+        for(int i = 0; i < n; i++){
+            String [] clothes = sc.nextLine().split(" ");
+            String type = clothes[1];
+            hMap.put(type, hMap.getOrDefault(type, 0) + 1);
+        }
+
+        int result = 1;
+
+        for(int value : hMap.values()){
+            result *= (value + 1);
+        }
+
+        System.out.println(result - 1);
+    }
+}
