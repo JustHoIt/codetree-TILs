e151bd6e8f37aed43eadfb8486282c6db02cb51b
박호민 <106057532+JustHoIt@users.noreply.github.com>
2024-05-11 20:10:34 +0900

    Title: xor 결과 최대 만들기, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

--- 240511/xor 결과 최대 만들기/max-of-xor.java
+++ 240511/xor 결과 최대 만들기/max-of-xor.java
@@ -1,30 +1,31 @@
 import java.util.Scanner;
 
 public class Main {
+    static int n, m;
+    static int [] arr;
+    static int max = 0;
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        int [] arr = new int[n];
+        n = sc.nextInt();
+        m = sc.nextInt();
+        arr = new int[n];
 
         for(int i = 0; i < n; i++){
             arr[i] = sc.nextInt();
         }
-
-        System.out.println(solution(arr, m, 0, 0));
+        solution(0, 0, 0);
+        System.out.println(max);
     }
 
-    static int solution(int [] arr, int m, int cur, int idx){
-        if(cur == m){
-            return idx;
+    static void solution(int start, int cnt, int xor){
+        if(cnt == m){
+            max = Math.max(max, xor);
+            return;
         }
 
-        int ans = 0;
-        for(int i = 0; i < arr.length; i++){
-            int max = idx ^ arr[i]; 
-            ans = Math.max(ans, solution(arr, m, cur + 1, max));
+        for(int i = start; i < n; i++){
+            solution(i + 1, cnt + 1, xor ^ arr[i]);
         }
-        return ans;
     }
 }
