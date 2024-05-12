3376aa9e762ad987efcc87e294b9fc0e50452a05
박호민 <106057532+JustHoIt@users.noreply.github.com>
2024-05-13 06:00:46 +0900

    Title: 2n개 중에 n개의 숫자를 적절하게 고르기, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

--- 240513/2n개 중에 n개의 숫자를 적절하게 고르기/choose-n-out-of-2n-properly.java
+++ 240513/2n개 중에 n개의 숫자를 적절하게 고르기/choose-n-out-of-2n-properly.java
@@ -1,28 +1,49 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int [] arr = new int[n * 2];
-
-        for(int i = 0; i < n * 2; i++){
-            arr[i] = sc.nextInt();
+    public static final int INT_MAX = Integer.MAX_VALUE;
+    public static final int MAX_N = 10;
+    
+    public static int n;
+    public static int[] num = new int[2 * MAX_N];
+    public static boolean[] visited = new boolean[2 * MAX_N];
+    
+    public static int ans = INT_MAX;
+    
+    public static int calc() {
+        int diff = 0;
+        for(int i = 0; i < 2 * n; i++){
+            diff = (visited[i]) ? (diff + num[i]) : (diff - num[i]);
         }
-        int diff = solution(arr, 0, 0, 0, n);
-        System.out.println(diff);
+        return Math.abs(diff);
+    }
+    
+    public static void findMin(int idx, int cnt) {
+        if(cnt == n) {
+            ans = Math.min(ans, calc());
+            return;
+        }
+        
+        if(idx == 2 * n){
+            return;
+        }
+
+        visited[idx] = true;
+        findMin(idx + 1, cnt + 1);
+        visited[idx] = false;
+        findMin(idx + 1, cnt);
     }
 
-    static int solution(int [] arr, int sum1, int sum2, int idx, int n){
-        if(idx == n * 2){
-            return Math.abs(sum1 - sum2);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        
+        for(int i = 0; i < 2 * n; i++){
+            num[i] = sc.nextInt();
         }
-
-        int diff1 = solution(arr, sum1 + arr[idx], sum2, idx + 1, n);
-
-        int diff2 = solution(arr, sum1, sum2 + arr[idx], idx + 1, n);
-
-        return Math.min(diff1, diff2);
+            
+        findMin(0, 0);
+        
+        System.out.print(ans);
     }
 }
