16047d01f1dd793fe7a9088f6a983e2ba66abcd2
박호민 <106057532+JustHoIt@users.noreply.github.com>
2024-05-15 03:41:50 +0900

    Title: 부분 수열의 합, Time: 158ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 240515/부분 수열의 합/the-sum-of-the-subsequences.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        boolean[][] dp = new boolean[n + 1][m + 1];
+        dp[0][0] = true;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= m; j++) {
+                dp[i][j] = dp[i - 1][j];
+                if (j >= arr[i - 1]) {
+                    dp[i][j] = dp[i][j] || dp[i - 1][j - arr[i - 1]];
+                }
+            }
+        }
+
+        System.out.println(dp[n][m] ? "Yes" : "No");
+    }
+}
