38830672a861debad3705ebff7108e3a9ebd503b
박호민 <106057532+JustHoIt@users.noreply.github.com>
2024-05-28 20:35:06 +0900

    Title: 정수 명령어, Time: 164ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240528/정수 명령어/Integer-command.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+import java.util.TreeSet;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        TreeSet<Integer> set = new TreeSet<>();
+
+        int n = sc.nextInt();
+
+        for(int i = 0; i < n; i++){ 
+            int k = sc.nextInt();
+            for(int j = 0;  j < k; j++){
+                String str = sc.next();
+                int num = sc.nextInt();
+                if(str.equals("I")){
+                    set.add(num);
+                }else{
+                    if(!set.isEmpty()){                        
+                        if(num == 1){
+                            set.remove(set.last());
+                        }else{
+                            set.remove(set.first());
+                        }
+                    }
+                }
+            }
+            if(set.isEmpty()){
+                System.out.println("EMPTY");
+            }else{
+                System.out.println(set.last() + " " + set.first());
+            }
+        }
+    }
+}
