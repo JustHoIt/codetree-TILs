0d862bc85069b6791942aaf4152e53c0945886bd
박호민 <106057532+JustHoIt@users.noreply.github.com>
2024-06-01 20:24:50 +0900

    Title: 자리 차지하기, Time: 1143ms, Memory: 62MB, Status: Passed - CodeTree

--- 240601/자리 차지하기/take-place.java
+++ 240601/자리 차지하기/take-place.java
@@ -3,28 +3,39 @@ import java.util.TreeSet;
 // n 안고자하는 의자 정보
 // m 의자번호(의자 개수)
 public class Main {
+    public static final int MAX = 1000000;
+
+    public static int n, m;
+    public static int[] arr = new int[MAX];
+
+    public static  TreeSet<Integer> set = new TreeSet<>();
+
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        TreeSet<Integer>[] set = new TreeSet[m + 1];
-
-        for(int i = 1; i <= m; i++){
-            set[i] = new TreeSet<>();
-        }
+        n = sc.nextInt();
+        m = sc.nextInt();
 
         for(int i = 0; i < n; i++){
-            int a = sc.nextInt();
-            set[a].add(i);
+            arr[i] = sc.nextInt();
         }
-        int cnt = 0;
+
         for(int i = 1; i <= m; i++){
-            if(!set[i].isEmpty()){
+            set.add(i);   
+        }
+
+        int cnt = 0;
+
+        for(int i = 0; i < n; i++){
+            if(set.floor(arr[i]) != null) {
+                int num = set.floor(arr[i]);
+                set.remove(num);
                 cnt++;
-                set[i].pollFirst();
+            }else{
+                break;
             }
         }
+
         System.out.println(cnt);
     }
 }
