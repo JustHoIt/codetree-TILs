02d690be30c7f9c643ee14f2f799b2cdccd83671
박호민 <106057532+JustHoIt@users.noreply.github.com>
2024-06-04 17:16:39 +0900

    Title: 배열 추출 2, Time: 138ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240604/배열 추출 2/array-extraction-2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.util.PriorityQueue;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        PriorityQueue<Integer> pQ = new PriorityQueue<>();
+
+        for(int i = 0; i < n; i++){
+            int num = sc.nextInt();
+
+            if(num == 0){
+                if(pQ.size() != 0){
+                    System.out.println(pQ.poll());
+                }else{
+                    System.out.println(0);
+                }
+            }else{
+                pQ.add(num);
+            }
+        }
+    }
+}
