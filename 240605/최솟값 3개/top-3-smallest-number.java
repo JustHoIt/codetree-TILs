4259c2d68e4800fae73d96a2edf6e8cd0f50faa8
박호민 <106057532+JustHoIt@users.noreply.github.com>
2024-06-05 04:20:49 +0900

    Title: 최솟값 3개, Time: 276ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240605/최솟값 3개/top-3-smallest-number.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+import java.util.PriorityQueue;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        PriorityQueue<Integer> pQ = new PriorityQueue<>();
+        List<Integer> qlist;
+
+        int n = sc.nextInt();
+
+        for(int i = 0; i < n; i++){
+            pQ.add(sc.nextInt());
+            int ans = 1;
+            if(pQ.size() >= 3){
+                qlist = new ArrayList<>(pQ);
+                qlist.sort(Integer::compare);
+                for(int j = 0; j < 3; j++){
+                    ans = ans * qlist.get(j);
+                }
+                System.out.println(ans);
+            }else{
+                System.out.println(-1);
+            }
+        }
+
+    }
+}
