4c7765ae76a39a154a7b82c8ff752e17a1d9f7bd
박호민 <106057532+JustHoIt@users.noreply.github.com>
2024-06-11 20:37:39 +0900

    Title: 최소 통과 시간, Time: 407ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240611/최소 통과 시간/minimum-transit-time.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] times = new int[m];
+
+        for (int i = 0; i < m; i++) {
+            times[i] = sc.nextInt();
+        }
+
+        long left = 1;
+        long right = 1000000000 * n;
+        long result = right;
+
+        while (left <= right) {
+            long mid = (left + right) / 2;
+            if (solution(n, times, mid)) {
+                result = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        System.out.println(result);
+    }
+
+    private static boolean solution(int n, int[] times, long timeLimit) {
+        long result = 0;
+
+        for (int time : times) {
+            result += timeLimit / time;
+            if (result >= n) {
+                return true;
+            }
+        }
+
+        return result >= n;
+    }
+}
