4ab15da638e5776e5969acc861aa94b31be0bc0f
박호민 <106057532+JustHoIt@users.noreply.github.com>
2024-06-16 17:49:43 +0900

    Title: 초대장과 번호표, Time: 1967ms, Memory: 107MB, Status: Passed - CodeTree

---
+++ 240616/초대장과 번호표/invitation-and-number-tag.java
@@ -0,0 +1,69 @@
+import java.util.Scanner;
+import java.util.Map;
+import java.util.List;
+import java.util.Set;
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.HashSet;
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int g = sc.nextInt();
+
+        Map<Integer, List<Integer>> groupsByPerson = new HashMap<>();
+        for (int i = 1; i <= n; i++) {
+            groupsByPerson.put(i, new ArrayList<>());
+        }
+
+        Map<Integer, Set<Integer>> notInvitedPeopleByGroup = new HashMap<>();
+        for (int i = 1; i <= g; i++) {
+            notInvitedPeopleByGroup.put(i, new HashSet<>());
+        }
+
+        for (int i = 1; i <= g; i++) {
+            int k = sc.nextInt();
+            for (int j = 0; j < k; j++) {
+                int person = sc.nextInt();
+
+                List<Integer> group = groupsByPerson.get(person);
+                group.add(i);
+
+                Set<Integer> notInvitedPeople = notInvitedPeopleByGroup.get(i);
+                notInvitedPeople.add(person);
+            }
+        }
+
+        int answer = 0;
+        boolean[] invited = new boolean[n + 1];
+        Queue<Integer> q = new LinkedList<>();
+        q.add(1);
+        invited[1] = true;
+
+        while (!q.isEmpty()) {
+            int person = q.poll();
+            answer++;
+
+            List<Integer> group = groupsByPerson.get(person);
+            for (int groupNumber : group) {
+                Set<Integer> notInvitedPeople = notInvitedPeopleByGroup.get(groupNumber);
+                notInvitedPeople.remove(person);
+
+                if (notInvitedPeople.size() == 1) {
+                    int remainPerson = new ArrayList<>(notInvitedPeople).get(0);
+                    if (!invited[remainPerson]) {
+                        q.add(remainPerson);
+                        invited[remainPerson] = true;
+                    }
+                }
+            }
+        }
+
+        System.out.print(answer);
+    }
+}
