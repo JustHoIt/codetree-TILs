6c8ce1f3b55b46ff171149a782fccdc30efe863e
박호민 <106057532+JustHoIt@users.noreply.github.com>
2024-07-08 19:09:58 +0900

    Title: 고대 보물 지도의 비밀, Time: 740ms, Memory: 42MB, Status: Passed - CodeTree

--- 240708/고대 보물 지도의 비밀/secret-of-ancient-treasure-map.java
+++ 240708/고대 보물 지도의 비밀/secret-of-ancient-treasure-map.java
@@ -1,40 +1,37 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        
-        int n = sc.nextInt();
-        int k = sc.nextInt();
-        int[] arr = new int[n];
-        
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-        }
-        
-        System.out.println(findMaxSum(arr, n, k));
-    }
+    public static final int MAXN = 100005;
+    public static final int INF = -1000000009;
+    public static int n, k;
+    public static int[] a = new int[MAXN];
+    public static int[][] dp = new int[MAXN][15];
 
-    public static int findMaxSum(int[] arr, int n, int k) {
-        int maxSum = Integer.MIN_VALUE;
-        int currentSum = 0;
-        int negativeCount = 0;
-        
-        int left = 0;
-        
-        for (int right = 0; right < n; right++) {
-            currentSum += arr[right];
-            if (arr[right] < 0) negativeCount++;
-            
-            while (negativeCount > k) {
-                if (arr[left] < 0) negativeCount--;
-                currentSum -= arr[left];
-                left++;
-            }
-            
-            maxSum = Math.max(maxSum, currentSum);
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        k = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            a[i] = sc.nextInt();
         }
-        
-        return maxSum;
+
+        int ans = INF;
+        for (int i = 1; i <= n; i++) {
+            if (a[i] >= 0) {
+                for (int j = 0; j <= k; j++) {
+                    dp[i][j] = Math.max(dp[i - 1][j] + a[i], dp[i][j]);
+                    ans = Math.max(ans, dp[i][j]);
+                }
+            }
+            else {
+                for (int j = 1; j <= k; j++) {
+                    dp[i][j] = Math.max(dp[i - 1][j - 1] + a[i], dp[i][j]);
+                    ans = Math.max(ans, dp[i][j]);
+                }
+            }
+        }
+
+        System.out.println(ans);
     }
 }
