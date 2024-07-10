fcc978693454ebc95c404f712649600e590bb8a4
박호민 <106057532+JustHoIt@users.noreply.github.com>
2024-07-10 20:21:36 +0900

    Title: 배열에서 자리 바꾸기, Time: 3615ms, Memory: 190MB, Status: Passed - CodeTree

--- 240710/배열에서 자리 바꾸기/switch-position-in-array.java
+++ 240710/배열에서 자리 바꾸기/switch-position-in-array.java
@@ -1,59 +1,78 @@
 import java.util.Scanner;
-import java.util.List;
-import java.util.LinkedList;
-import java.util.ArrayList;
 
 public class Main {
+    public static final int MAX_N = 250005;
+
+    static class Node {
+        int id;
+        Node prev, next;
+
+        Node(int id) {
+            this.id = id;
+            this.prev = null;
+            this.next = null;
+        }
+    }
+
+    public static Node[] nodes = new Node[MAX_N];
+    public static void connect(Node s, Node e) {
+        if (s != null)
+            s.next = e;
+        if (e != null)
+            e.prev = s;
+    }
+    public static void swapSubarray(Node a, Node b, Node c, Node d) {
+        
+        Node after_prevA = c.prev;
+        Node after_nextB = d.next;
+
+        Node after_prevC = a.prev; 
+        Node after_nextD = b.next;
+        if (b.next == c) {
+            after_prevA = d;
+            after_nextD = a;
+        }
+        if (d.next == a) {
+            after_nextB = c;
+            after_prevC = b;
+        }
+
+        connect(after_prevA, a);
+        connect(b, after_nextB);
+
+        connect(after_prevC, c);
+        connect(d, after_nextD);
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
-        int N = sc.nextInt();
-        int Q = sc.nextInt();
-        
-        LinkedList<Integer> list = new LinkedList<>();
-        for (int i = 1; i <= N; i++) {
-            list.add(i);
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++){
+            nodes[i] = new Node(i);
         }
-        
-        for (int i = 0; i < Q; i++) {
+
+        for (int i = 1; i < n; i++){
+            connect(nodes[i], nodes[i + 1]);
+        }
+
+        int q = sc.nextInt();
+
+        for (int i = 0; i < q; i++) {
             int a = sc.nextInt();
             int b = sc.nextInt();
             int c = sc.nextInt();
             int d = sc.nextInt();
-            
-            swapSubarrays(list, a, b, c, d);
+
+            swapSubarray(nodes[a], nodes[b], nodes[c], nodes[d]);
         }
-        
-        for (int num : list) {
-            System.out.print(num + " ");
+        Node cur = nodes[1];
+        while (cur.prev != null){
+            cur = cur.prev;
+            }
+        while (cur != null) {
+            System.out.print(cur.id + " ");
+            cur = cur.next;
         }
-    }
-    
-    private static void swapSubarrays(LinkedList<Integer> list, int a, int b, int c, int d) {
-        List<Integer> subarray1 = extractSubarray(list, a, b);
-        List<Integer> subarray2 = extractSubarray(list, c, d);
-        
-        int index1 = list.indexOf(a);
-        int index2 = list.indexOf(c);
-        
-        for (int i = 0; i < subarray1.size(); i++) {
-            list.set(index1 + i, subarray2.get(i));
-        }
-        
-        for (int i = 0; i < subarray2.size(); i++) {
-            list.set(index2 + i, subarray1.get(i));
-        }
-    }
-    
-    private static List<Integer> extractSubarray(LinkedList<Integer> list, int startVal, int endVal) {
-        int startIndex = list.indexOf(startVal);
-        int endIndex = list.indexOf(endVal);
-        
-        List<Integer> subarray = new ArrayList<>();
-        for (int i = startIndex; i <= endIndex; i++) {
-            subarray.add(list.get(i));
-        }
-        
-        return subarray;
     }
 }
