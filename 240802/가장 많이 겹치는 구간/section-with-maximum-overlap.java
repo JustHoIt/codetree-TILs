6a3070657c7baea3b7948b9d63ef5da729fdd84a
박호민 <106057532+JustHoIt@users.noreply.github.com>
2024-08-02 18:39:49 +0900

    Title: 가장 많이 겹치는 구간, Time: 1188ms, Memory: 53MB, Status: Passed - CodeTree

---
+++ 240802/가장 많이 겹치는 구간/section-with-maximum-overlap.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {    
+    public static final int MAX_R = 200000;
+    public static final int MAX_N = 100000;
+    public static int n;
+    public static int[] x1 = new int[MAX_N];
+    public static int[] x2 = new int[MAX_N];
+    public static int[] checked = new int[MAX_R + 1];
+    public static int ans;
+
+    public static void main(String[] args) {
+        //여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        
+        for(int i = 0; i < n; i++) {
+            x1[i] = sc.nextInt();
+            x2[i] = sc.nextInt();
+        }
+
+        for(int i = 0; i < n; i++) {
+            checked[x1[i]]++;
+            checked[x2[i]]--;
+        }
+
+        int overlappedCnt = 0;
+
+        for(int i = 1; i <= MAX_R; i++) {
+            overlappedCnt += checked[i];
+            ans = Math.max(ans, overlappedCnt);
+        }
+        
+        System.out.print(ans);
+    }
+}
