2d39a445eab8efa36169aab1207cf52b90c644a9
박호민 <106057532+JustHoIt@users.noreply.github.com>
2024-10-31 16:31:45 +0900

    Title: 가장 가까운 거리의 최댓값, Time: 2045ms, Memory: 95MB, Status: Passed - CodeTree

---
+++ 241031/가장 가까운 거리의 최댓값/maximum-of-nearest-distance.java
@@ -0,0 +1,109 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+
+class Node {
+    int index, dist;
+    
+    public Node(int index, int dist) {
+        this.index = index;
+        this.dist = dist;
+    }
+};
+
+class Element implements Comparable<Element> {
+    int dist, index;
+    
+    public Element(int dist, int index) {
+        this.dist = dist;
+        this.index = index;
+    }
+
+    @Override
+    public int compareTo(Element e) {
+        return this.dist - e.dist;
+    }
+};
+
+public class Main {
+    public static final int INT_MIN = Integer.MIN_VALUE;
+    public static final int INT_MAX = Integer.MAX_VALUE;
+    public static final int MAX_N = 100000;
+    public static int n, m;
+    public static int a, b, c;
+    public static ArrayList<Node>[] graph = new ArrayList[MAX_N + 1];
+    public static PriorityQueue<Element> pq = new PriorityQueue<>();
+    
+    public static int[] dist = new int[MAX_N + 1];
+    public static int[] abcDist = new int[MAX_N + 1];
+    
+    public static int ans = INT_MIN;
+    public static void dijkstra(int k) {
+        for(int i = 1; i <= n; i++){
+            dist[i] = (int)1e9;
+        }
+        dist[k] = 0;
+    
+        pq.add(new Element(0, k));
+    
+        while(!pq.isEmpty()) {
+            int minDist = pq.peek().dist;
+            int minIndex = pq.peek().index;
+            pq.poll();
+            if(minDist != dist[minIndex]){
+                continue;
+            }
+
+            for(int j = 0; j < graph[minIndex].size(); j++) {
+                int targetIndex = graph[minIndex].get(j).index;
+                int targetDist = graph[minIndex].get(j).dist;
+                int newDist = dist[minIndex] + targetDist;
+
+                if(dist[targetIndex] > newDist) {
+                    dist[targetIndex] = newDist;
+                    pq.add(new Element(newDist, targetIndex));
+                }
+            }
+        }
+        for(int i = 1; i <= n; i++){
+            abcDist[i] = Math.min(abcDist[i], dist[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = sc.nextInt();
+
+        for(int i = 1; i <= n; i++){
+            graph[i] = new ArrayList<>();
+        }
+
+        while(m-- > 0) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            int z = sc.nextInt();
+
+            graph[x].add(new Node(y, z));
+            graph[y].add(new Node(x, z));
+        }
+
+        for(int i = 1; i <= n; i++){
+            abcDist[i] = INT_MAX;
+        }
+
+        dijkstra(a);
+        dijkstra(b);
+        dijkstra(c);
+
+        for(int i = 1; i <= n; i++){
+            ans = Math.max(ans, abcDist[i]);
+        }
+
+        System.out.print(ans);
+    }
+}
